4a1f2b8e06e28223d4260d123bde2118ee627d9b
Vincent <wenzhishen@icloud.com>
2017-04-25 11:09:28 +0800

    oc version finished

--- OC/GifBackgroundViewDemo/GifBackgroundViewDemo/ViewController.m
+++ OC/GifBackgroundViewDemo/GifBackgroundViewDemo/ViewController.m
@@ -9,21 +9,25 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (weak, nonatomic) IBOutlet UIWebView *webView;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    [self loadHTML];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)loadHTML {
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"background" ofType:@"html"];
+    NSURL *url = [NSURL fileURLWithPath:path];
+    NSData *data = [NSData dataWithContentsOfURL:url];
+    NSURL *baseURL = [url URLByDeletingLastPathComponent];
+    [self.webView loadData:data MIMEType:@"text/html" textEncodingName:@"utf-8" baseURL:baseURL];
 }
 
-
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
 @end
