55620612b88dde068a6d14157f78823feb2af179
Vincent <wenzhishen@icloud.com>
2017-04-25 10:51:19 +0800

    Swift

---
+++ Swift/GifBackgroundViewDemo/GifBackgroundViewDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GifBackgroundViewDemo
+//
+//  Created by Vincent on 4/25/17.
+//  Copyright © 2017 zssr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
